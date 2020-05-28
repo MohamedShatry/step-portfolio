dcb4115a04756117c19ae9ca94687c365390501f
Mohamed <mhmshatry@gmail.com>
2020-05-27 23:30:40 -0400

    Add dynamic content loading into DOM

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,26 +12,136 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- * Adds a random greeting to the page.
- */
-function addRandomGreeting() {
-    
-  let returnedRepos = [];
 
-  fetch('https://api.github.com/users/MohamedShatry/repos')
-  .then(res => res.json())
-  .then(repos => {
-      repos.forEach(repo => {
-          returnedRepos.push({
-              title: repo.name,
-              url: repo.html_url,
-              language: repo.language,
-              description: repo.description
-          });
-      });
-  })
-  .catch(err => console.log(err));
 
-  console.log(returnedRepos);
+async function getData (){
+    let response = await fetch('https://api.github.com/users/MohamedShatry/repos');
+    var resJSON = await response.json();
+    let varHeader = SetHeader(resJSON[0].owner);
+    injectHeader(varHeader);
+
+    resJSON.forEach(repo => {
+        var returnRepo = SetRepos(repo);
+        injectRepo(returnRepo);
+    });
 }
+
+
+function SetHeader(repo){
+    var header = {
+        title : repo.login,
+        url: repo.html_url,
+        avatar_url: repo.avatar_url
+    }
+
+    return header;
+}
+
+function SetRepos(repo){
+    repo_content = {
+        title: repo.name,
+        url: repo.html_url,
+        language: repo.language,
+        description: repo.description,
+        web: repo.homepage
+    }
+
+    return repo_content;
+};
+
+function injectHeader(header){
+    const gitHeader = document.getElementById("git-header");
+	
+    const avatar_link = document.createElement("a");
+    avatar_link.href = header.url;
+    avatar_link.target = "_blank";
+
+    const avatar = document.createElement("img");
+    avatar.classList.add("avatar");
+    avatar.src = header.avatar_url;
+    
+	avatar_link.appendChild(avatar);
+
+    const title_tag = document.createElement("b");
+    title_tag.classList.add("git-header-title")
+    const title = document.createTextNode(header.title);
+    title_tag.appendChild(title);
+    
+    gitHeader.appendChild(avatar_link);
+    gitHeader.appendChild(title_tag);
+
+}
+
+function injectRepo(repo){
+    const main = document.getElementById("repo-container");
+
+	//Create container to hold content parsed in
+    const repoContainer = document.createElement("div");
+    repoContainer.classList.add("repo-content")
+	
+    //Create Container heading
+    const title_tag = document.createElement("p");
+    title_tag.classList.add("repo-header")
+    const title = document.createTextNode(repo.title);
+    title_tag.appendChild(title);
+	
+    //Create container description
+    const description_tag = document.createElement("p");
+    const description = document.createTextNode(repo.description);
+    description_tag.appendChild(description);
+	
+    //Create div that will hold the language of each repo and the associated links 
+    const bottom_div = document.createElement("div");
+    bottom_div.classList.add("lowest-div");
+	
+	//Create containet for the language
+    const language_tag = document.createElement("p");
+    language_tag.classList.add("lowest-tag");
+    const language = document.createTextNode(repo.language);
+    language_tag.appendChild(language);
+	
+    //Create container that will link to the Github
+    const url_link = document.createElement("a");
+    url_link.href = repo.url;
+    url_link.target = "_blank";
+
+    const url_tag = document.createElement("p");
+    url_tag.classList.add("lowest-tag");
+    url_tag.classList.add("arrow-link");
+    
+    const url_text = document.createTextNode("View Code→");
+    url_tag.appendChild(url_text);
+    url_link.appendChild(url_tag);
+
+
+    //Append everything to the bottom div
+    bottom_div.appendChild(language_tag);
+    bottom_div.appendChild(url_link);
+    
+    //Conditionally create container if repo has a web url
+	if(repo.web !== null){
+        const web_link = document.createElement("a");
+        web_link.href = repo.web;
+        web_link.target = "_blank";
+
+        const web_tag = document.createElement("p");
+    	web_tag.classList.add("lowest-tag");
+    	web_tag.classList.add("arrow-link");
+    	
+    	const web_text = document.createTextNode("View Live→");
+    	web_tag.appendChild(web_text);
+        web_link.appendChild(web_tag);
+        bottom_div.appendChild(web_link);
+    }
+
+	//Append everything to the repo container
+    repoContainer.appendChild(title_tag);
+    repoContainer.appendChild(description_tag);
+    repoContainer.appendChild(bottom_div);
+
+    main.appendChild(repoContainer);
+
+}
+
+
+getData();
