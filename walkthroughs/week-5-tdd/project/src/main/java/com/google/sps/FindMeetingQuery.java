19e5250da833cac3a6d10e9acc821ab19df7cc22
Mohamed <mhmshatry@gmail.com>
2020-06-16 20:32:39 +0000

    Add functionality to return available timeslots to fit request

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,71 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.List;
 
 public final class FindMeetingQuery {
+  
+  public final Collection<TimeRange> slots = new ArrayList<>();
+  public final List<TimeRange> bookedTimes = new ArrayList<>();
+
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    // throw new UnsupportedOperationException("TODO: Implement this method.");
+
+    if(request.getDuration() >= TimeRange.WHOLE_DAY.duration()){
+        return slots;
+    }else if(events.isEmpty()){
+        slots.add(TimeRange.WHOLE_DAY);
+    }else {
+        for(Event event: events){
+            for(String attendee: request.getAttendees()){
+                if(event.getAttendees().contains(attendee)){
+                    bookedTimes.add(event.getWhen());
+                    break;
+                }
+            }
+        }
+
+        if(bookedTimes.size() == 0){
+            slots.add(TimeRange.WHOLE_DAY);
+            return slots;
+        }
+
+        //Sort
+        Collections.sort(bookedTimes, TimeRange.ORDER_BY_START);
+
+        //Merge
+        for (int i = 0; i < bookedTimes.size() - 1; i++){
+            if(bookedTimes.get(i).overlaps(bookedTimes.get(i+1)) || bookedTimes.get(i).contains(bookedTimes.get(i+1))){
+                if(bookedTimes.get(i).end() > bookedTimes.get(i+1).end()){
+                    bookedTimes.set(i, TimeRange.fromStartEnd(bookedTimes.get(i).start(), bookedTimes.get(i).end(), false));
+                }else{
+                    bookedTimes.set(i, TimeRange.fromStartEnd(bookedTimes.get(i).start(), bookedTimes.get(i+1).end(), false));
+                }
+                bookedTimes.remove(i+1);
+            }
+        }
+
+        //Get everthing in between
+        long duration = request.getDuration();
+        
+        if((bookedTimes.get(0).start() - TimeRange.START_OF_DAY) >= duration){
+            slots.add(TimeRange.fromStartEnd(TimeRange.START_OF_DAY, bookedTimes.get(0).start(), false));
+        }
+
+        for(int i = 0; i < bookedTimes.size() - 1; i++){
+            if((bookedTimes.get(i+1).start() - bookedTimes.get(i).end()) >= duration){
+                slots.add(TimeRange.fromStartEnd(bookedTimes.get(i).end(), bookedTimes.get(i+1).start(), false));
+            }
+        }
+        
+        if((TimeRange.END_OF_DAY - bookedTimes.get(bookedTimes.size() - 1).end()) >= duration){
+            slots.add(TimeRange.fromStartEnd(bookedTimes.get(bookedTimes.size() - 1).end(), TimeRange.END_OF_DAY, true));
+        }
+    }
+
+    return slots;
   }
+  
 }
